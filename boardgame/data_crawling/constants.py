36eab32b7a5ccf3342b38b23685d4c4ad02e738f
kdchoi-mkt <valuekds@naver.com>
2021-02-27 17:57:31 +0900

    construct crawler class

---
+++ boardgame/data_crawling/constants.py
@@ -0,0 +1,12 @@
+PAGE_MAX = 5
+
+BASE_LINK = "https://boardgamegeek.com/browse/boardgame/page"
+TABLE_TAG = '#row_'
+
+API_LINK = "https://boardgamegeek.com/xmlapi/boardgame/"
+DESCRIPTION_TAG = 'description'
+MECHANIC_TAG = 'boardgamemechanic'
+CATEGORY_TAG = 'boardgamecategory'
+THEME_TAG = 'boardgamesubdomain'
+
+OWNER_TAG = 'owned'
