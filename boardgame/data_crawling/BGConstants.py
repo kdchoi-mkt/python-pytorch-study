1f013f09bd114162279f1d581031e546efb6d3da
kdchoi-mkt <valuekds@naver.com>
2021-02-28 15:58:38 +0900

    Changed constants module name because of conflict

---
+++ boardgame/data_crawling/BGConstants.py
@@ -0,0 +1,14 @@
+PAGE_MAX = 5
+
+BASE_LINK = "https://boardgamegeek.com/browse/boardgame/page"
+TABLE_TAG = "#row_"
+
+API_LINK = "https://boardgamegeek.com/xmlapi/boardgame/"
+DESCRIPTION_TAG = "description"
+MECHANIC_TAG = "boardgamemechanic"
+CATEGORY_TAG = "boardgamecategory"
+THEME_TAG = "boardgamesubdomain"
+
+QUERY_STR = "stats=1"
+# The following tags require the `stats=1` query string
+OWNER_TAG = "owned"
