99f1921fd3f8b4efabc39db01e243474c2fd1ffb
kdchoi-mkt <valuekds@naver.com>
2021-03-03 22:12:43 +0900

    Change File Dir

--- boardgame/modeling/util/__init__.py
+++ boardgame/modeling/util/__init__.py
@@ -1,3 +1,3 @@
 from modeling.util.Item2Vec import Item2Vec
-from modeling.item_based.SkipZeroMLP import SkipZeroMLP
+from modeling.util.SkipZeroMLP import SkipZeroMLP
 from modeling.util.TrainingVisualization import TrainVisualize
