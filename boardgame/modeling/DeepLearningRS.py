150bb085d83ae651cc6d5b7f70bd398c9cc48b7e
kdchoi-mkt <valuekds@naver.com>
2021-03-03 16:17:12 +0900

    Add _find_index to treat CFBasedRS

--- boardgame/modeling/DeepLearningRS.py
+++ boardgame/modeling/DeepLearningRS.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+# The model is inherited by CF based RS
+from RecommendationSystem import CFBasedRS
+
 # Data Handle
 import numpy as np
 import pandas as pd
@@ -20,6 +23,7 @@ class SkipZeroMLP(nn.Module):
     When user did not check product P_1's preference even if he like it,
     the optimizer and loss function learn the user dislike P_1.
     Therefore, before the end of the calculation, the module filters the value when it is originally 0.
+    If you want to treat non-filtered outcome, you should extract `self.model` instance from the `SkipZeroMLP`.
     """
 
     def __init__(self, input_size, output_size, original_result):
@@ -41,7 +45,7 @@ class SkipZeroMLP(nn.Module):
         return self.model(input) * non_missing_output
 
 
-class DeepLearningRS(TrainVisualize):
+class DeepLearningRS(CFBasedRS, TrainVisualize):
     """The collaborative filtering can be implemented by one-hidden-layer perceptron with non-activation model.
     This means that the MLP deep learning is the generalization of the collaborative filtering.
 
@@ -63,19 +67,22 @@ class DeepLearningRS(TrainVisualize):
         self.value_col = value_col
         self.iteration = iteration
 
-        self.label_encoding = LabelEncoder().fit(self.data_frame[user_col])
+        self.user_label_encoding = LabelEncoder().fit(self.data_frame[user_col])
+        self.item_label_encoding = LabelEncoder().fit(self.data_frame[item_col])
 
         self.user_num = self.data_frame[self.user_col].nunique()
         self.item_num = self.data_frame[self.item_col].nunique()
 
     def derive_user_encoding_tensor(self):
         """This is used to input tensor"""
-
         return torch.ones(self.user_num).diag()
 
     def derive_item_rating_tensor(self):
-        """This is used to output tensor"""
+        """This is used to output tensor.
 
+        Because data_frame.pivot() sorts index and columns automatically, we does not need to treat orders and encoding.
+        (In fact, the labeling also works based on the names' order)
+        """
         rating_data = self.data_frame.pivot(
             columns=self.item_col, index=self.user_col, values=self.value_col
         ).fillna(0)
@@ -91,3 +98,9 @@ class DeepLearningRS(TrainVisualize):
         self.cost_module = nn.MSELoss
 
         return self.training_data(input_tensor, output_tensor).model(input_tensor)
+
+    def _find_user_index(self, name):
+        return self.user_label_encoding.transform([name])[0]
+
+    def _find_item_index(self, name):
+        return self.item_label_encoding.transform([name])[0]
