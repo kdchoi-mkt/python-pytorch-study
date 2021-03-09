1394bcf31832810a7a2ef4efe81af2637fea29b3
kdchoi-mkt <valuekds@naver.com>
2021-03-09 16:00:13 +0900

    Add docstring to the IFRS

--- boardgame-recommendation/modeling/collaborative_filtering/ImplicitFeedbackRS.py
+++ boardgame-recommendation/modeling/collaborative_filtering/ImplicitFeedbackRS.py
@@ -35,13 +35,25 @@ class ImplicitFeedbackRS(DeepLearningRS):
         )
 
     def generate_recommend_matrix(self):
+        """In the implicit feedback recommendation system, the objective function is something changed.
+
+        Let the predictive preference for given item i on user u as p_iu.
+        f_iu is the indicator whether the user left their footprint (provided implicit feedback).
+        c_iu is the confidence that can be written by c_iu = {confidence} * r_iu + 1, where r_iu is the actual provided implicit feedback number.
+
+        Then, the objective function Obj(Model) is defined by the following formula
+        Obj(Model) = Sum{c_iu * (f_iu - p_iu) ^ 2}
+
+        The objection function is similar to nn.MSELoss, but the parameter is different.
+
+        Therefore, I used `ConfidenceMSELoss` and `SkipZeroMLP` to resolve the objection problem."""
         input_tensor = self.derive_user_encoding_tensor()
         output_tensor = self.derive_item_rating_tensor()
 
         confidence_tensor = output_tensor * self.confidence
         output_tensor = (output_tensor > 0) * 1
 
-        self.cost_module = ConfidenceMSELossModule(confidence_tensor).get_module()
+        self.cost_module = ConfidenceMSELossModule(confidence_tensor + 1).get_module()
 
         self.learning_model = SkipZeroMLP(self.user_num, self.item_num, output_tensor)
 
